8dfdd0058f0e117ac9274e60cde9dccc6316876f
Vijay <vijay.8085@gmail.com>
2020-06-14 08:44:39 +0530

    test

--- SQL/Table/Customer.sql
+++ SQL/Table/Customer.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[Customer]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    [customer_name] VARCHAR(50) NOT NULL
+    [customer_name] VARCHAR(60) NOT NULL, 
+    [customer_address] VARCHAR(200) NULL
 )
